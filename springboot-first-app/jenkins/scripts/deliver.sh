4939736ee51d89e3b07ad1ef4935becb0f3ad604
Stanisław Skrzypek <01143844@pw.edu.pl>
2021-05-15 11:19:58 +0200

    modify deliver script

--- springboot-first-app/jenkins/scripts/deliver.sh
+++ springboot-first-app/jenkins/scripts/deliver.sh
@@ -22,5 +22,9 @@ set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+echo 'SPRAWDZENIE'
+echo ${NAME}
+echo ${VERSION}
+echo 'KONIEC SPRAWDZENIA'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
